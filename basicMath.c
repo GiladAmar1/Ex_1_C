aad3add4f1d3b62dfe70098e18e072a1080b6f3d
gilad <giladamar1212@gmail.com>
2020-11-12 12:23:33 +0200

    power+basicMath

---
+++ basicMath.c
@@ -0,0 +1,21 @@
+#include "myMath.h"
+float add(float x, float y)
+{
+	float func=x+y;
+	return (func);
+}
+float sub(float x, float y)
+{
+	float func=x-y;
+	return(func);
+}
+double mul(double x, int y)
+{
+	double func=x*y;
+	return (func);
+}
+double div(double x, int y)
+{
+	double func=x/y;
+	return(func);
+}
---
+++ power.c
@@ -0,0 +1,22 @@
+#include "myMath.h"
+double Pow(double x, int y)
+{
+	double sum=1;
+	while (y!=0) {
+		if (y>0) {
+			sum=sum*x;
+			y--;
+		}
+		else if (y<0) {
+			sum=sum/x;
+			y++;
+		}
+	}
+	return sum;
+}
+double Exp(int x)
+{
+	double e=2.71828182846;
+	double func= Pow(e, x);
+	return(func);
+}
