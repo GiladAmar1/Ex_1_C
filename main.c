b98c4c62af7037de03097f9239762ab69bf5e526
gilad <giladamar1212@gmail.com>
2020-11-14 21:04:28 +0200

    Finsh Ex_1

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int main()
 	printf("Please inset a real number:\n");
     scanf("%lf", &x);
 	double func1=sub(add(Exp((int)(x)), Pow(x, 3)), 2);
-	printf("The value of f(𝑥)=𝑒^𝑥 +x^3 −2 at the point: %f is: %f\n", x, func1);
+	printf("The value of f(𝑥)=𝑒^𝑥 +x^3 −2 at the point: %.4f is: %.4f\n", x, func1);
 	double func2 = add(mul(x, 3), mul(Pow(x, 2), 2));
-	printf("The value of f(x)=3x+2X^2 at the point :%f is: %f\n", x, func2);
+	printf("The value of f(x)=3x+2X^2 at the point :%.4f is: %.4f\n", x, func2);
 	double func3 = sub(div(mul((Pow(x, 3)), 4), 5), mul(x, 2));
-	printf("The value of f(x)=(3x^2)/5-2x atthe point: %f is: %f\n", x, func3);
+	printf("The value of f(x)=(3x^2)/5-2x atthe point: %.4f is: %.4f\n", x, func3);
 	return 0;
 }
